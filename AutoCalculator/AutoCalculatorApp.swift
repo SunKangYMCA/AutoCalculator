85f78bf2f83f8dd5c535b4edc58283a9df0ae0ac
SunKang <coolshark248@gmail.com>
2023-06-16 10:33:57 -0400

    creat file

---
+++ AutoCalculator/AutoCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoCalculatorApp.swift
+//  AutoCalculator
+//
+//  Created by 강성찬 on 2023-06-16.
+//
+
+import SwiftUI
+
+@main
+struct AutoCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
